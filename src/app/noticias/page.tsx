dc246b7ec6606bfa8874ba1631070421bbfc3563
EuduardoP <dudupiresrosa@gmail.com>
2025-04-09 02:29:45 -0300

    🐛 Corrige erro de Suspense para useSearchParams

--- src/app/noticias/page.tsx
+++ src/app/noticias/page.tsx
@@ -1,7 +1,12 @@
 import { getNewsData } from "@/lib/get-news-data"
 import { NoticiasClient } from "./noticiasClient"
+import { Suspense } from "react"
 
 export default async function NoticiasPage() {
 	const news = await getNewsData()
-	return <NoticiasClient news={news} />
+	return (
+		<Suspense fallback={<div className="w-full p-8 text-center">Carregando...</div>}>
+			<NoticiasClient news={news} />
+		</Suspense>
+	)
 }
